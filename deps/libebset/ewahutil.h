4ce2813b43333a6cd018fc4cef45060bad6ab25f
ncloudioj <njiang028@gmail.com>
2014-03-19 15:52:31 -0400

    Fix len() of pyebset

--- deps/libebset/ewahutil.h
+++ deps/libebset/ewahutil.h
@@ -41,15 +41,48 @@
 
 
 
+static inline uint32_t ctz64(uint64_t n) {
+#if defined(__GNUC__) && UINT_MAX >= UINT32_MAX
+	return static_cast<uint32_t>(__builtin_ctzl(n));
+#elif defined(_MSC_VER) && _MSC_VER >= 1400
+	uint32_t i;
+	_BitScanForward64((unsigned long *) &i, n);
+	return i;
+#else
+	uint32_t i = 1;
+	if ((n & static_cast<uint64_t>(4294967295)) == 0) {
+		n >>= 32;
+		i += 32;
+	}
+	if ((n & static_cast<uint64_t>(0x0000FFFFUL)) == 0) {
+		n >>= 16;
+		i += 16;
+	}
+
+	if ((n & static_cast<uint64_t>(0x000000FFUL)) == 0) {
+		n >>= 8;
+		i += 8;
+	}
+
+	if ((n & static_cast<uint64_t>(0x0000000FUL)) == 0) {
+		n >>= 4;
+		i += 4;
+	}
+
+	if ((n & static_cast<uint64_t>(0x00000003UL)) == 0) {
+		n >>= 2;
+		i += 2;
+	}
+    i -= (n & 0x1);
+	return i;
+#endif
+}
 
 
 
 
 static inline uint32_t ctz32(uint32_t n) {
-#if defined(__INTEL_COMPILER)
-	return _bit_scan_forward(n);
-
-#elif defined(__GNUC__) && UINT_MAX >= UINT32_MAX
+#if defined(__GNUC__) && UINT_MAX >= UINT32_MAX
 	return static_cast<uint32_t>(__builtin_ctz(n));
 
 #elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
@@ -63,30 +96,29 @@ static inline uint32_t ctz32(uint32_t n) {
 	return i;
 
 #else
-	uint32_t i = 0;
+	uint32_t i = 1;
 
-	if ((n & UINT32_C(0x0000FFFF)) == 0) {
+	if ((n & static_cast<uint32_t>(0x0000FFFF)) == 0) {
 		n >>= 16;
-		i = 16;
+		i += 16;
 	}
 
-	if ((n & UINT32_C(0x000000FF)) == 0) {
+	if ((n & static_cast<uint32_t>(0x000000FF)) == 0) {
 		n >>= 8;
 		i += 8;
 	}
 
-	if ((n & UINT32_C(0x0000000F)) == 0) {
+	if ((n & static_cast<uint32_t>(0x0000000F)) == 0) {
 		n >>= 4;
 		i += 4;
 	}
 
-	if ((n & UINT32_C(0x00000003)) == 0) {
+	if ((n & static_cast<uint32_t>(0x00000003)) == 0) {
 		n >>= 2;
 		i += 2;
 	}
 
-	if ((n & UINT32_C(0x00000001)) == 0)
-		++i;
+    i -= (n & 1);
 
 	return i;
 #endif
@@ -94,10 +126,7 @@ static inline uint32_t ctz32(uint32_t n) {
 
 
 static inline uint32_t ctz16(uint16_t n) {
-#if defined(__INTEL_COMPILER)
-	return _bit_scan_forward(n);
-
-#elif defined(__GNUC__) && UINT_MAX >= UINT32_MAX
+#if defined(__GNUC__) && UINT_MAX >= UINT32_MAX
 	return static_cast<uint32_t>(__builtin_ctz(n));
 
 #elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
@@ -111,30 +140,23 @@ static inline uint32_t ctz16(uint16_t n) {
 	return i;
 
 #else
-	uint32_t i = 0;
+	uint32_t i = 1;
 
-	if ((n & UINT16_C(0x0000FFFF)) == 0) {
-		n >>= 16;
-		i = 16;
-	}
-
-	if ((n & UINT16_C(0x000000FF)) == 0) {
+	if ((n & static_cast<uint16_t>(0x000000FF)) == 0) {
 		n >>= 8;
 		i += 8;
 	}
 
-	if ((n & UINT16_C(0x0000000F)) == 0) {
+	if ((n & static_cast<uint16_t>(0x0000000F)) == 0) {
 		n >>= 4;
 		i += 4;
 	}
 
-	if ((n & UINT16_C(0x00000003)) == 0) {
+	if ((n & static_cast<uint16_t>(0x00000003)) == 0) {
 		n >>= 2;
 		i += 2;
 	}
-
-	if ((n & UINT16_C(0x00000001)) == 0)
-		++i;
+    i -= (n & 1);
 
 	return i;
 #endif
@@ -150,24 +172,39 @@ static inline uint32_t ctz16(uint16_t n) {
 inline uint32_t countOnes(uint32_t x) {
     return static_cast<uint32_t>(__builtin_popcount(x));
 }
-#else
+#elif defined(_MSC_VER) && _MSC_VER >= 1400
 inline uint32_t countOnes(uint32_t x) {
-  uint32_t c; // c accumulates the total bits set in v
-  for (c = 0; x; c++) {
-     x &= x - 1; // clear the least significant bit set
-  }
-  return c;
+	return __popcnt(x);
+}
+#else
+inline uint32_t countOnes(uint32_t v) {
+    v = v - ((v >> 1) & 0x55555555);
+    v = (v & 0x33333333) + ((v >> 2) & 0x33333333);
+    return static_cast<uint32_t>((((v + (v >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24);
 }
 #endif
 
 
+#ifdef __GNUC__
 /**
  * count the number of bits set to one (64 bit version)
  */
-inline uint32_t countOnes(uint64_t v) {
-    return countOnes(static_cast<uint32_t> (v)) + countOnes(
-            static_cast<uint32_t> (v >> 32));
+inline uint32_t countOnes(uint64_t x) {
+    return static_cast<uint32_t>(__builtin_popcountl(x));
 }
+#elif defined(_MSC_VER) && _MSC_VER >= 1400
+inline uint32_t countOnes(uint32_t x) {
+	return static_cast<uint32_t>(__popcnt64(static_cast<__int64>(x)));
+}
+#else
+inline uint32_t countOnes(uint64_t v) {
+    v = v - ((v >> 1) & 0x5555555555555555);
+    v = (v & 0x3333333333333333) +
+        ((v >> 2) & 0x3333333333333333);
+    v = ((v + (v >> 4)) & 0x0F0F0F0F0F0F0F0F);
+    return static_cast<uint32_t>((v*(0x0101010101010101))>>56);
+}
+#endif
 
 inline uint32_t countOnes(uint16_t v) {
     return countOnes(static_cast<uint32_t>(v));
@@ -181,10 +218,8 @@ inline uint32_t numberOfTrailingZeros(uint32_t x) {
 
 
 inline uint32_t numberOfTrailingZeros(uint64_t x) {
-    if(static_cast<uint32_t> (x)!= 0) {
-        return numberOfTrailingZeros(static_cast<uint32_t> (x));
-    }
-    else return 32+numberOfTrailingZeros(static_cast<uint32_t> (x >> 32));
+    if (x == 0) return 64;
+    return ctz64(x);
 }
 
 inline uint32_t numberOfTrailingZeros(uint16_t x) {
