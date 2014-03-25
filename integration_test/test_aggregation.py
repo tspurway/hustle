b9e46b9560872820248747c38c1e1af38955eed4
Tim Spurway <tspurway@gmail.com>
2014-03-25 14:54:18 -0400

    resetting...

--- integration_test/test_aggregation.py
+++ integration_test/test_aggregation.py
@@ -15,52 +15,52 @@ class TestAggregation(unittest.TestCase):
     def tearDown(self):
         pass
 
-    # def test_simple_aggregation(self):
-    #     imps = Table.from_tag(IMPS)
-    #     res = select(imps.ad_id, imps.cpm_millis, where=imps.date == '2014-01-27')
-    #     results = [c for c, _ in result_iterator(res)]
-    #
-    #     sum_millis = {}
-    #     for ad_id, millis in results:
-    #         if ad_id not in sum_millis:
-    #             sum_millis[ad_id] = [0, 0]
-    #         sum_millis[ad_id][0] += millis
-    #         sum_millis[ad_id][1] += 1
-    #
-    #     res = select(imps.ad_id, h_sum(imps.cpm_millis), h_count(), where=imps.date == '2014-01-27')
-    #     results = [c for c, _ in result_iterator(res)]
-    #     self.assertGreater(len(results), 0)
-    #     for ad_id, millis, count in results:
-    #         ad_tup = sum_millis[ad_id]
-    #         self.assertEqual(millis, ad_tup[0])
-    #         self.assertEqual(count, ad_tup[1])
-    #
-    # def test_ordered_aggregation(self):
-    #     imps = Table.from_tag(IMPS)
-    #     res = select(imps.ad_id, imps.cpm_millis, where=imps.date == '2014-01-27')
-    #     resx = [c for c, _ in result_iterator(res)]
-    #
-    #     sum_millis = {}
-    #     for ad_id, millis in resx:
-    #         if ad_id not in sum_millis:
-    #             sum_millis[ad_id] = [0, 0]
-    #         sum_millis[ad_id][0] += millis
-    #         sum_millis[ad_id][1] += 1
-    #
-    #     res = select(imps.ad_id, h_sum(imps.cpm_millis), h_count(),
-    #                  where=imps.date == '2014-01-27',
-    #                  order_by=2,
-    #                  limit=3)
-    #     results = [c for c, _ in result_iterator(res)]
-    #     self.assertGreater(len(results), 0)
-    #     lowest = 0
-    #     for ad_id, millis, count in results:
-    #         self.assertLessEqual(lowest, count)
-    #         lowest = count
-    #         ad_tup = sum_millis[ad_id]
-    #         self.assertEqual(millis, ad_tup[0])
-    #         self.assertEqual(count, ad_tup[1])
-    #     self.assertTrue(len(results) == min(len(sum_millis), 3))
+    def test_simple_aggregation(self):
+        imps = Table.from_tag(IMPS)
+        res = select(imps.ad_id, imps.cpm_millis, where=imps.date == '2014-01-27')
+        results = [c for c, _ in result_iterator(res)]
+
+        sum_millis = {}
+        for ad_id, millis in results:
+            if ad_id not in sum_millis:
+                sum_millis[ad_id] = [0, 0]
+            sum_millis[ad_id][0] += millis
+            sum_millis[ad_id][1] += 1
+
+        res = select(imps.ad_id, h_sum(imps.cpm_millis), h_count(), where=imps.date == '2014-01-27')
+        results = [c for c, _ in result_iterator(res)]
+        self.assertGreater(len(results), 0)
+        for ad_id, millis, count in results:
+            ad_tup = sum_millis[ad_id]
+            self.assertEqual(millis, ad_tup[0])
+            self.assertEqual(count, ad_tup[1])
+
+    def test_ordered_aggregation(self):
+        imps = Table.from_tag(IMPS)
+        res = select(imps.ad_id, imps.cpm_millis, where=imps.date == '2014-01-27')
+        resx = [c for c, _ in result_iterator(res)]
+
+        sum_millis = {}
+        for ad_id, millis in resx:
+            if ad_id not in sum_millis:
+                sum_millis[ad_id] = [0, 0]
+            sum_millis[ad_id][0] += millis
+            sum_millis[ad_id][1] += 1
+
+        res = select(imps.ad_id, h_sum(imps.cpm_millis), h_count(),
+                     where=imps.date == '2014-01-27',
+                     order_by=2,
+                     limit=3)
+        results = [c for c, _ in result_iterator(res)]
+        self.assertGreater(len(results), 0)
+        lowest = 0
+        for ad_id, millis, count in results:
+            self.assertLessEqual(lowest, count)
+            lowest = count
+            ad_tup = sum_millis[ad_id]
+            self.assertEqual(millis, ad_tup[0])
+            self.assertEqual(count, ad_tup[1])
+        self.assertTrue(len(results) == min(len(sum_millis), 3))
 
     def test_multiple_group_bys(self):
         imps = Table.from_tag(IMPS)
