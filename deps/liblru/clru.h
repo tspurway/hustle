8416d937c29f18e196d22161d95d0c7b29204006
Philippe Ombredanne <pombredanne@gmail.com>
2014-05-28 11:01:17 +0200

    Updating notice to Tim Day's full MIT notice
    Per http://timday.bitbucket.org/lru.html#x1-30002 the actual notice should be:
    ```
    Copyright (c) 2010-2011, Tim Day <timday@timday.com> 
     
    Permission to use, copy, modify, and/or distribute this software for any 
    purpose with or without fee is hereby granted, provided that the above 
    copyright notice and this permission notice appear in all copies. 
     
    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES 
    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF 
    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR 
    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES 
    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN 
    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF 
    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
    ```

--- deps/liblru/clru.h
+++ deps/liblru/clru.h
@@ -4,6 +4,10 @@
  *  Based on the 'LRU cache implementation in C++' article by Tim Day.
  *  Copyright (c) 2010-2011, Tim Day <timday@timday.com>
  *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
