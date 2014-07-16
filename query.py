86f4efc23c02437e3cbef9855a9f1de6b495ad39
ncloudioj <njiang028@gmail.com>
2014-07-16 13:44:45 -0400

    Remove query.py

--- query.py
+++
@@ -1,4 +0,0 @@
-
-
-select(i.token, min(i.time), where=((i.campaign_io_id == 17146) |(i.campaign_io_id == 17147) |(i.campaign_io_id == 17160)) & (i.impressions > 0) & (i.is_psa == 0) & (i.date == '2014-02-20'), nest=True)
-
